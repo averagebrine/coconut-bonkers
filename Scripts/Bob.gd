1b766cbb3ab2b01fd5756d96c4f297b55c6e8b3f
averagebrine <84639405+averagebrine@users.noreply.github.com>
2023-02-12 17:25:20 -0500

    improved movement

--- Scripts/Bob.gd
+++ Scripts/Bob.gd
@@ -1,23 +1,32 @@
 extends KinematicBody2D
 
-var movementSpeed : float = 800
-
-var vel : Vector2 = Vector2()
 onready var sprite : Sprite = get_node("Sprite")
+var movementSpeed : float = 400
+var movementSmooth : float = 0.2
+
+var velocity : Vector2 = Vector2()
+var targetVelocity : Vector2 = Vector2()
 
 func _physics_process(_delta):
+	move()
+
+func move():
+	velocity = move_and_slide(lerp(velocity, targetVelocity, movementSmooth), Vector2.UP)
 	
-	vel.x = 0
-	vel.y = 0
+	targetVelocity = Vector2.ZERO
 	
+	# horizontal movement
 	if(Input.is_action_pressed("move_left")):
-		vel.x -= movementSpeed
-	if(Input.is_action_pressed("move_right")):
-		vel.x += movementSpeed
-	if(Input.is_action_pressed("move_up")):
-		vel.y -= movementSpeed
-	if(Input.is_action_pressed("move_down")):
-		vel.y += movementSpeed
+		targetVelocity.x -= movementSpeed
+	elif(Input.is_action_pressed("move_right")):
+		targetVelocity.x += movementSpeed
+	else:
+		targetVelocity.x = 0
 	
-	# apply velocity
-	vel = move_and_slide(vel, Vector2.UP)
+	# vertical movement
+	if(Input.is_action_pressed("move_up")):
+		targetVelocity.y -= movementSpeed
+	elif(Input.is_action_pressed("move_down")):
+		targetVelocity.y += movementSpeed
+	else:
+		targetVelocity.y = 0
