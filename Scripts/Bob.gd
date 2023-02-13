a50439c2c3606adbfc12919b411840e5fd500e26
averagebrine <84639405+averagebrine@users.noreply.github.com>
2023-02-12 23:19:52 -0500

    added new testing level, animated bob

--- Scripts/Bob.gd
+++ Scripts/Bob.gd
@@ -1,16 +1,27 @@
 extends KinematicBody2D
 
 onready var sprite : Sprite = get_node("Sprite")
-var movementSpeed : float = 400
+onready var animator : AnimationPlayer = get_node("Animator")
+var movementSpeed : float = 200
 var movementSmooth : float = 0.2
 
 var velocity : Vector2 = Vector2()
 var targetVelocity : Vector2 = Vector2()
 
+func _process(_delta):
+	
+	# animation
+	if(abs(velocity.x) > 100 || abs(velocity.y) > 100):
+		animator.play("run")
+	else:
+		animator.play("idle")
+
 func _physics_process(_delta):
 	move()
 
 func move():
+	
+	# make it SMOOTH
 	velocity = move_and_slide(lerp(velocity, targetVelocity, movementSmooth), Vector2.UP)
 	
 	targetVelocity = Vector2.ZERO
