e98885f55f6e9869ccb396be5e4f7f5ade1de88e
averagebrine <84639405+averagebrine@users.noreply.github.com>
2023-02-13 17:42:39 -0500

    added coconut carrying mechanics,  edited window, revised levels to fit window

--- Scripts/Bob.gd
+++ Scripts/Bob.gd
@@ -2,17 +2,29 @@ extends KinematicBody2D
 
 onready var sprite : Sprite = get_node("Sprite")
 onready var animator : AnimationPlayer = get_node("Animator")
+onready var radius : Area2D = get_node("InteractionRadius")
 var movementSpeed : float = 200
 var movementSmooth : float = 0.2
 
 var velocity : Vector2 = Vector2()
 var targetVelocity : Vector2 = Vector2()
+var holdingCoconut : bool = false
+
+signal placeCoconut
 
 func _process(_delta):
-	
+		
+	if Input.is_action_just_pressed("interact"):
+		interact()
+
 	# animation
-	if(abs(velocity.x) > 100 || abs(velocity.y) > 100):
-		animator.play("run")
+	if abs(velocity.x) > 100 || abs(velocity.y) > 100:
+		if(holdingCoconut):
+			animator.play("run_coconut")
+		else:
+			animator.play("run")
+	elif holdingCoconut:
+		animator.play("idle_coconut")
 	else:
 		animator.play("idle")
 
@@ -27,17 +39,51 @@ func move():
 	targetVelocity = Vector2.ZERO
 	
 	# horizontal movement
-	if(Input.is_action_pressed("move_left")):
+	if Input.is_action_pressed("move_left"):
 		targetVelocity.x -= movementSpeed
-	elif(Input.is_action_pressed("move_right")):
+	elif Input.is_action_pressed("move_right"):
 		targetVelocity.x += movementSpeed
 	else:
 		targetVelocity.x = 0
 	
 	# vertical movement
-	if(Input.is_action_pressed("move_up")):
+	if Input.is_action_pressed("move_up"):
 		targetVelocity.y -= movementSpeed
-	elif(Input.is_action_pressed("move_down")):
+	elif Input.is_action_pressed("move_down"):
 		targetVelocity.y += movementSpeed
 	else:
 		targetVelocity.y = 0
+		
+func interact():
+	if holdingCoconut:
+		drop()
+		return
+	
+	var nearby = radius.get_overlapping_areas()
+	if !nearby.empty():
+		if holdingCoconut:
+			return
+
+		var nearbyCoconuts = []
+		for obj in nearby:
+			if obj.is_in_group("coconuts"):
+				nearbyCoconuts.append(obj)
+		
+		var nearestCoconut = null
+		var nearestDist = 0
+		for coconut in nearbyCoconuts:
+			var dist = position.distance_to(coconut.position)
+			if nearestCoconut == null or dist < nearestDist:
+				nearestCoconut = coconut
+				nearestDist = dist
+				
+		nearestCoconut.queue_free()
+		holdingCoconut = true
+
+func drop():
+	# spawn a coconut at player position
+	emit_signal("placeCoconut")
+	
+	holdingCoconut = false
+	
+	# look for nearby snakes, if there are any, bonk them and consume the coconut
---
+++ Scripts/Game.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+onready var player = get_node("Bob")
+var coconutOffset = Vector2(0, 48)
+
+func _ready():
+	player.connect("placeCoconut", self, "_placeCoconut")
+
+func _placeCoconut():
+	var coconutScene = load("res://Scenes/Coconut.tscn")
+	var coconutInstance = coconutScene.instance()
+	add_child(coconutInstance)
+	coconutInstance.set_global_position(player.get_global_position() + coconutOffset)
